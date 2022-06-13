3de47e8293ea5a157d570c84f1e098fc1e5f180c
teixerya <78224021+teixerya@users.noreply.github.com>
2022-06-13 17:40:17 -0400

    Add files via upload

---
+++ A1Q2/festImage.js
@@ -0,0 +1,48 @@
+function showImage(){
+
+    var txt= "(Alexander Raths, 2020)";
+
+    document.getElementById("image").src="halloween.png"
+
+    document.getElementById("hallo").innerHTML=
+        "Figure:1 Halloween 2020" + "<br>" + "Home Page"
+        + txt.link("https://www.almanac.com/content/when-is-halloween")
+
+    + "<br>" + "<br>"+ "Halloween takes place on October 31st."
+    + "<br>" +"On Halloween, partakers dress up in costumes."
+    + "<br>" + "For example:"+ "<br>"+
+    "Firefighter, Cat woman, and movie characters."
+    + "<br>" + "<br>" +" Children can go door to door in their costume" +
+    " and collect candy from their neighbours.";
+}
+
+function showImage2(){
+
+    var txt2= "(Homemade, 2020)";
+    document.getElementById("img2").src="LatinFestival.png"
+
+    document.getElementById("Latin").innerHTML=
+    "Figure 1:Homemade Gifts"
+    + "<br>" + "Home page"
+    + txt2.link("https://www.todocanada.ca/summer-festivals-toronto/")
+    + "<br>" + "<br>" +"Latin Fest is a 3 day event in September that"
+    +" celebrates hispanic culture."
+    + "<br>" + "The festival will showcase Latin music at the"
+     + " Jazz Bistro in Toronto."
+    + "<br>" + "Attendees can expect Latin jazz at the festival.";
+}
+
+function showImage3(){
+    var txt3= "(To do Canada, 2018)";
+    document.getElementById("img3").src="NewYear.png"
+
+    document.getElementById("Ny").innerHTML=
+    "Figure 1: Summer Festivals"
+    + "<br>" + "Home page"
+    + txt3.link("https://www.homemade-gifts-made-easy.com/new-year-wishes.html")
+    + "<br>" + "<br>" +"New Year takes place on the last day of the year"
+    +" and is celebrated till the first day of the new year."
+    + "<br>" + "Liberty Grand is a castle in Toronto"
+    + "that is used to celebrate new year."
+    + "<br>" + "Attendees can expect a formal crowd and a young crowd";
+}
